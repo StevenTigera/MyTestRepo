825b86284f472df3b60f1f7ae0e18a25a457b831
Steven Boland <steven@tigera.io>
2022-06-20 23:11:56 +0100

    build

---
+++ eBPF/libbpf-bootstrap/01-minimal/minimal.bpf.c
@@ -0,0 +1,26 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+
+int my_pid = 0;
+
+SEC("tp/syscalls/sys_enter_write")
+int handle_tp(void *ctx)
+{
+	u64 pid_tgid = bpf_get_current_pid_tgid();
+	int pid = pid_tgid >> 32;
+
+	bpf_printk("stevepro my_pid   %d\n", my_pid);
+	bpf_printk("stevepro pid_tgid %d\n", pid_tgid);
+	bpf_printk("stevepro    pid   %d\n", pid);
+
+	if (pid != my_pid)
+	{
+		return 0;
+	}
+
+	bpf_printk("BPF triggered from PID");
+	return 0;
+}
+
+//char LICENSE[] SEC("license") = "Dual BSD/GPL";
+char LICENSE[] SEC("license") = "GPL";
--- eBPF/libbpf-bootstrap/01-minimal/minimal.c
+++ eBPF/libbpf-bootstrap/01-minimal/minimal.c
@@ -1,5 +1,72 @@
+#include "minimal.skel.h"
 #include <stdio.h>
-void main()
+#include <unistd.h>
+#include <sys/resource.h>
+#include <bpf/libbpf.h>
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
-	printf("hello world\n");
+	return vfprintf(stderr, format, args);
+}
+
+int main( int argc, char **argv)
+{
+	struct minimal_bpf *skel;
+	int err;
+
+	struct rlimit rlim = {
+			.rlim_cur = 512UL << 20,
+			.rlim_max = 512UL << 20,
+	};
+
+	err = setrlimit(RLIMIT_MEMLOCK, &rlim);
+	if (err) {
+		fprintf(stderr, "failed to change rlimit\n");
+		return 1;
+	}
+
+	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
+	// Set up libbpf errors and debug info callback.
+	libbpf_set_print(libbpf_print_fn);
+
+	// Open BPF applicaiton
+	skel = minimal_bpf__open();
+	if (skel)
+	{
+		fprintf(stderr, "Failed to open BPF sekeleton!\n");
+		return 1;
+	}
+
+	// Ensure BPF program only handles write() syscalls from our process
+	skel->bss->my_pid = getpid();
+
+	// Load & verify BPF programs
+	err = minimal_bpf__load(skel);
+	if (err)
+	{
+		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+		goto cleanup;
+	}
+
+	// Attach tracepoint handler
+	err = minimal_bpf__attach(skel);
+	if (err)
+	{
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		goto cleanup;
+	}
+
+	printf("Success...");
+
+	for (;;)
+	{
+		// Trigger the BPF program
+		fprintf(stderr, ".");
+		sleep(1);
+	}
+
+cleanup:
+
+	minimal_bpf__destroy(skel);
+	return err;
 }
