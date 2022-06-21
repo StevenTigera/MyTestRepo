ce569a07adca905c81dc7da1543da9f34d7250bd
Steven Boland <steven@tigera.io>
2022-06-21 17:07:31 +0100

    repo bpf

--- eBPF/libbpf-bootstrap/01-minimal/minimal.bpf.c
+++ eBPF/libbpf-bootstrap/01-minimal/minimal.bpf.c
@@ -1,26 +1,20 @@
-#include "vmlinux.h"
+
+#include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
 int my_pid = 0;
 
 SEC("tp/syscalls/sys_enter_write")
 int handle_tp(void *ctx)
 {
-	u64 pid_tgid = bpf_get_current_pid_tgid();
-	int pid = pid_tgid >> 32;
-
-	bpf_printk("stevepro my_pid   %d\n", my_pid);
-	bpf_printk("stevepro pid_tgid %d\n", pid_tgid);
-	bpf_printk("stevepro    pid   %d\n", pid);
-
+	int pid = bpf_get_current_pid_tgid() >> 32;
 	if (pid != my_pid)
 	{
 		return 0;
 	}
 
-	bpf_printk("BPF triggered from PID");
+	bpf_printk("is BPF triggered from PID %d!!\n", pid);
 	return 0;
 }
-
-//char LICENSE[] SEC("license") = "Dual BSD/GPL";
-char LICENSE[] SEC("license") = "GPL";
